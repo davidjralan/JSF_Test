f6a6f2b30ca6d65896e198c6802b1931e720107c
davidjralan <31747613+davidjralan@users.noreply.github.com>
2017-09-07 16:15:49 -0700

    Release1

--- JavaServerFaces/src/main/java/com/mkyong/common/HelloBean.java
+++ JavaServerFaces/src/main/java/com/mkyong/common/HelloBean.java
@@ -23,6 +23,6 @@ public class HelloBean implements Serializable {
 	
 	public static void main(String[] args) {
 		System.out.println("new method main");
-	
+		System.out.println("Release1");
 	}
 }
